379bffe0a0942a87eb455c606eca322034bc43df
Himanshu Kumawat <013himanshu@gmail.com>
2020-08-22 22:06:53 +0530

    Add files via upload

---
+++ assets/keynotes.php
@@ -0,0 +1,130 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<title>KEYNOTES | SIN 2017</title>
+	<?php require 'php_includes/imports.php'; ?>
+</head>
+
+<body class="profile-page">
+<!-- Navbar -->
+<?php require 'php_includes/navbar.php'; ?>
+<!-- End Navbar -->
+
+<div class="wrapper">
+	<div class="header header-filter" style="background-image:url(assets/img/background/mujBg.jpg);"></div>
+	<div class="main main-raised">
+		<section class="section speaker text-center">
+			<div class="container">
+				<h2 class="title">KEYNOTES</h2>				
+				
+<!-- <p style="text-align: justify;"><span style="color: #003366; font-size: 14pt;"><strong>Keynote Talks:</strong></span></p> -->
+<p style="text-align: left;"><img width="150" height="250" style="margin: 5px 5px 8px 0px; float: left;" alt="Suraj C. Kothari-150x2250-border" src="assets/img/committee/kothari.gif" />&nbsp;<a name="Suraj"></a><strong>Prof. Suraj C. Kothari</strong></p>
+<div style="text-align: left;">&nbsp;Richardson Chair Professor, Electrical and Computer Engineering, Iowa State University President, EnSoft Corp </div>
+<div style="text-align: left;">
+<p style="text-align: left;"><strong>Title</strong></p>
+<div style="text-align: left;">Software Security Headaches: Analgesic or Hospital?</div>
+<div style="text-align: left;">&nbsp;</div>
+<strong>Abstract</strong></div>
+<div style="text-align: left;">&nbsp;</div>
+<div style="text-align: justify;">
+The talk will be about analysis of complex security problems in large software - the problems where analgesic will not get rid of the headache. The need for automated analysis is clear given the enormous software. More than 100 feet stack of paper is needed to print software like the Android operating system.  Researchers are grappling with automation, from machine learning to formal verification, to analyze software. The practitioners are in a quandary what research to believe.
+Patching security holes with reactive automation is not the solution when we face zero-day software defects with deadly power for catastrophic destruction. Not only must automated software security analysis scale to large software but also be proactive and highly accurate.  
+This talk will reflect on: what should be the automation for software security analysis and why? It will be reflections based on years of experience of analyzing software with millions of lines of code, and leading large research projects to develop automated software analysis tools for cybersecurity.
+
+<div style="text-align: left;">&nbsp;<strong>Bio</strong></div>
+<div style="text-align:  justify;">&nbsp;Suresh Kothari is the Richardson Professor of Electrical and Computer Engineering (ECE) at Iowa State University (ISU). He has pioneered research on machine-enabled reasoning to solve complex problems of software productivity, security and safety. <br>He served as a Principal Investigator (PI) for the US Defense Advanced Research Project Agency (DARPA) Automated Program Analysis for Cybersecurity (APAC) program, and a Co-PI for the DARPA Software Enabled Control (SEC) program. Currently he is a PI for the DARPA Space/Time Analysis for Cybersecurity (STAC) program. </br> EnSoft (http://www.ensoftcorp.com/), the company he founded in 2002, provides software productivity, safety, and security products and services worldwide to more than 330 organizations including all major avionics and automobile companies. He was awarded in 2012 the Iowa State Board of Regents Professor Award for excellence in research, teaching, and service. He has served as a Distinguished ACM Lecturer. He has given more than 100 invited talks worldwide at major conferences, government organizations, universities, and industry.</div>
+<div style="text-align: left;">&nbsp;</div>
+<div style="text-align: left;">&nbsp;</div>
+<div style="text-align: left;"><img width="150" height="250" style="margin: 5px 5px 8px 0px; float: left;" alt="Akka Zemmari" src="assets/img/committee/akka.jpg" />
+<p><strong><a name="Akka"></a>Prof. Akka Zemmari</strong></p>
+</div>
+<div style="text-align: left;">Associate Professor, Université de Bordeaux, France &nbsp;</div>
+<div style="text-align: left;">Researcher at Laboratoire Bordelais de Recherche en Informatique (LaBRI)</div>
+<p style="text-align: left;"><strong>Title</strong></p>
+<div style="text-align: left;">-</div>
+<div style="text-align: left;">&nbsp;<strong>Abstract</strong></div>
+
+<div style="text-align: left;"><strong>Bio</strong></div>
+<div style="text-align: left;">&nbsp;----</div>
+<p style="text-align: justify;">&nbsp;</p>
+
+<p style="text-align: left;"><img width="150" height="250" style="margin: 5px 5px 8px 0px; float: left;" alt="Mohamed MOSBAH" src="/assets/img/committee/Mohamed.jpg" />&nbsp;<a name="Mohamed MOSBAH"></a><strong>Prof. Mohamed MOSBAH</strong></p>
+
+<div style="text-align: left;">&nbsp;Professor  at  Institut Polytechnique de Bordeaux  (ENSEIRB-MATMECA)</div>
+<div style="text-align:left;">&nbsp;Director of Industrial Relations at LaBRI and ENSEIRB-MATMECA</div>
+<p style="text-align: left;"><strong>&nbsp;Title</strong></p>
+<div style="text-align: left;">&nbsp;-</div>
+<div style="text-align: left;">
+<p><strong>Abstract</strong></p>
+</div>
+<div style="text-align: left;">&nbsp;-</div>
+<div style="text-align: left;">&nbsp;<strong>Bio</strong></div>
+<div style="text-align: justify;">&nbsp; Prof. Mohamed Mosbah is a  professor in Computer Science at  Polytechnic Institute of Bordeaux (high engineering school), university of Bordeaux, since 2005. He has done his research work in LaBRI (computer science laboratory of the university of Bordeaux ). His research inter are Distributed Computing, Distributed System, Distribute Algorithm,Mobile and Ad hoc networks Graph transformations for designing algorithms Formal methods and Security of distributed systems.</div>
+<br> </br>
+
+<div style="text-align: left;"><img width="150" height="250" style="margin: 5px 5px 8px 0px; float: left;" alt="Priyadarsi Nanda-206x195-bordered" src="/assets/img/committee/Priyadarsi-Nanda.jpg" />
+<p><strong><a name="Priyadarsi Nanda"></a>Priyadarsi Nanda</strong></p>
+</div>
+<div style="text-align: left;">Senior Lecturer, University of Technology Sydney&nbsp;</div>
+<div style="text-align: left;">
+Core Member,  INEXT - Innovation in IT Services and Applications </div>
+<div style="text-align: left;">Core Member,  CRIN - Centre for Realtime Information Networks </div>
+<p style="text-align: left;"><strong>Title</strong></p>
+<div style="text-align: left;">-</div>
+<div style="text-align: left;">&nbsp;</div>
+<div style="text-align: left;"><strong>Abstract</strong></div>
+<div style="text-align: left;">&nbsp;</div>
+<div style="text-align: left;">&nbsp;-</div>
+<div style="text-align: left;">&nbsp;</div>
+<div style="text-align: left;"><strong>Bio</strong></div>
+<div style="text-align: justify;">&nbsp;Dr Nanda joined UTS in 2001. His roles have combined research, teaching and administration. Dr. Nanda is a core research member of Centre for Real-time Information Networks (CRIN) and Research Centre for Innovation in IT Services and Applications (iNEXT). Dr Nanda has expanded his research activities through active research student supervision, collaborating with academics and industry researchers in the area of Cyber Security, IOT Security, network Quality of Service (QoS), assisted health care using sensor networks, and wireless sensor networks. Dr. Nanda is the Program Director of He is currently supervising Six PhD students. Dr. Nanda has published over 60 refereed high quality research publications and many of them appear in Tier A and Tier A* Journals and Conferences</div>
+
+<br> </br>
+
+<div style="text-align: left;"><img width="150" height="250" style="margin: 5px 5px 8px 0px; float: left;" alt="Mauro Conti-206x195-bordered" src="/assets/img/committee/Mauro-Conti.jpg" />
+<p><strong><a name="Mauro Conti"></a>Mauro Conti</strong></p>
+</div>
+<div style="text-align: left;">Associate Professor, University of Padua&nbsp;</div>
+<div style="text-align: left;">EU Marie Curie Fellow </div>
+<p style="text-align: left;"><strong>Title</strong></p>
+<div style="text-align: left;">-</div>
+<div style="text-align: left;">&nbsp;</div>
+<div style="text-align: left;"><strong>Abstract</strong></div>
+<div style="text-align: left;">&nbsp;</div>
+<div style="text-align: left;">&nbsp;-</div>
+
+<div style="text-align: left;"><strong>Bio</strong></div>
+<div style="text-align: justify;">&nbsp;Prof. Mauro Conti received MSc and PhD in Computer Science (advisor Prof. Luigi V. Mancini) from Sapienza University of Rome, Italy , in 2005 and 2009, respectively. In 2008, he was Visiting Researcher (supervised by Prof. Sushil Jajodia) at the Center for Secure Information Systems (CSIS) at George Mason University, Fairfax, VA, USA . In 2009 he was selected for the ERCIM (European Research Consortium for Informatics and Mathematics) "Alain Bensoussan" Fellowship (currently a EU Marie Curie COFUND action) . From 2009 to 2011 he was Postdoctoral Researcher (supervised by Prof. Andrew S. Tanenbaum and Prof. Bruno Crispo) at Vrije Universiteit Amsterdam, The Netherlands. His research interests are mainly in the area of security and privacy. In this area, he published more than 120 papers in topmost international peer-reviewed journals and conferences, including IEEE TIFS, IEEE TDSC, IEEE TPDS, ACM TWEB, ACM/IEEE TON, IEEE TSC, IEEE COMST, ACM CCS, Usenix Security, ACM AsiaCCS, ACM WiSec, ACM SACMAT, ACM MobiHoc, ACNS, IEEE ICDCS, and ESORICS. He is Associate Editor for  IEEE Communications Surveys & Tutorials and IEEE Transactions on Information Forensics and Security, and he served as Program Committee member of several conferences, including ACM AsiaCCS, ACM WiSec, ACM CODASPY, ACM SACMAT, IEEE INFOCOM, IEEE CNS, IEEE PASSAT, IEEE MASS, and ACNS. He was panelist at ACM CODASPY 2011, and panel chair at ICISS '16. He was General Chair for SecureComm 2012 and ACM SACMAT 2013, and Program Chair for TRUST 2015, ICISS '16, and WiSec '17.</div>
+
+<p style="text-align: justify;">&nbsp;</p>
+	
+</div>
+					<!-- --------------------------------------------- -->
+					
+		</section>
+		
+		<!-- Social Links -->
+		<?php require 'php_includes/social.php'; ?>
+
+	</div>
+</div>
+<!-- Footer -->
+<?php require 'php_includes/footer.php'; ?>
+</body>
+<!--   Core JS Files   -->
+<script src="assets/js/jquery.min.js" type="text/javascript"></script>
+<script src="assets/js/bootstrap.min.js" type="text/javascript"></script>
+<script src="assets/js/material.min.js"></script>
+
+<!--  Plugin for the Sliders, full documentation -->
+<script src="assets/js/nouislider.min.js" type="text/javascript"></script>
+
+<!-- Control Center for activating the ripples, parallax effects, scripts from the example pages etc -->
+<script src="assets/js/material-kit.js" type="text/javascript"></script>
+<script type="text/javascript">
+	$().ready(function(){
+		// the body of this function is in assets/material-kit.js
+		$(window).on('scroll', materialKit.checkScrollForTransparentNavbar);
+	});
+</script>
+</html>
